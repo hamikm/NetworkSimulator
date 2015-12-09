36cdd99257eb988b1070e3b7a110aa0a9247d06d
Jessica_B_Li <eilica668@gmail.com>
2015-12-09 14:37:55 -0800

    Enabled interactively selecting which links to display on graph.

--- plot/plotNetSimData.py
+++ plot/plotNetSimData.py
@@ -13,6 +13,11 @@ measured and then graphed as a time trace and overall average:
         -- packet delay
 The simulator records data into a .json file, from which data is extracted
 and graphed.
+
+For each subplot in each window (Link Metric, Flow Metric), the viewer can
+choose which data set to view by clicking on the associated line 'symbol'
+in the subplot's legend. This feature is most helpful when comparing link
+flow rates, since the data sets all overlap and become hard to make sense of.
 '''
 
 import json
@@ -64,6 +69,18 @@ def loadAllData(filename):
 
 	return (times, linksData, flowsData)
 
+def makeLegDict(legend, plottedLines):
+	'''
+	Helper function that helps create a dictionary that connects a plotted line
+	to its reference in the legent itself.
+	Inputs should be a legend handle and an array of handles to plotted lines
+	'''
+	linker = {}
+	for legLine, plotLine in zip(legend.get_lines(), plottedLines):
+		legLine.set_picker(5)    # set tolerance
+		linker[legLine] = plotLine
+
+	return linker
 
 def plotLinkData(time, linksData):
 	'''
@@ -73,41 +90,70 @@ def plotLinkData(time, linksData):
 
 	f, (lrates, buffocc, pktloss) = plt.subplots(3, 1, sharex=True)
 
-	# set window title
-	f.canvas.set_window_title("Flow Metrics Graph")
+	# For the purpose of linking each plotted Data Set to its line in the
+	# legend so that user can interactively select which data to view
+	linkLines = []
+	bufocLines = []
+	pktloLines = []
 	
 	# plot link rates, buff occup, packet loss for all links
 	# note: 'o' is to ensure scatterplot
 	for link in linksData.keys():
-		lrates.plot(time, linksData[link]["Link Rate (Mbps)"], '-',
+		lhandle, = lrates.plot(time, linksData[link]["Link Rate (Mbps)"], '-',
 				label=str(link),
 				markersize=2, markeredgewidth=0.0)
-		buffocc.plot(time, linksData[link]["Buffer Occupancy (KB)"], '-',
+		bohandle, = buffocc.plot(time, linksData[link]["Buffer Occupancy (KB)"], '-',
 				label=str(link),
 				markersize=2, markeredgewidth=0.0)
-		pktloss.plot(time, linksData[link]["Packet Loss (pkts)"], '-',
+		plhandle, = pktloss.plot(time, linksData[link]["Packet Loss (pkts)"], '-',
 				label=str(link),
 				markersize=2, markeredgewidth=0.0)
 	
+		# update array of handles to plotted lines
+		linkLines.append(lhandle)
+		bufocLines.append(bohandle)
+		pktloLines.append(plhandle)
+
 	# add titles, labels and legend; set lower bound of y axis
 	lrates.set_xlabel('Time (ms)')
 	lrates.set_ylabel('Link Rate (Mbps)')
-	lrates.legend(loc='best')
+	lleg = lrates.legend(loc='best')
 	lrates.set_ylim(ymin=0)
 	
 	buffocc.set_xlabel('Time (ms)')
 	buffocc.set_ylabel('Buffer Occupancy (KB)')
-	buffocc.legend(loc='best')
+	boleg = buffocc.legend(loc='best')
 	buffocc.set_ylim(ymin=0)
 	
 	pktloss.set_xlabel('Time (ms)')
 	pktloss.set_ylabel('Packet Loss (pkts)')
-	pktloss.legend(loc='best')
+	pleg = pktloss.legend(loc='best')
 	pktloss.set_ylim(ymin=0)
 
 	# set window title
 	f.canvas.set_window_title("Link Metrics Graph")
 
+	# create dictionary connecting line in legend to plot
+	linkDict = makeLegDict(lleg, linkLines)
+	bfDict = makeLegDict(boleg, bufocLines)
+	plDict = makeLegDict(pleg, pktloLines)
+
+	def onpick(event):
+	    # on the pick event, find the orig line corresponding to the
+	    # legend proxy line, and toggle the visibility
+	    legline = event.artist
+	    origline = linkDict[legline]
+	    vis = not origline.get_visible()
+	    origline.set_visible(vis)
+	    # Change the alpha on the line in the legend so we can see what lines
+	    # have been toggled
+	    if vis:
+	        legline.set_alpha(1.0)
+	    else:
+	        legline.set_alpha(0.2)
+	    f.canvas.draw()
+
+	f.canvas.mpl_connect('pick_event', onpick)
 
 def plotFlowData(time, flowsData):
 	'''
@@ -115,39 +161,72 @@ def plotFlowData(time, flowsData):
 	generate three plots: 1 showing each metric for all flows.
 	'''
 	f, (frates, winsize, pktdelay) = plt.subplots(3, 1, sharex=True)
+
+	# For the purpose of linking each plotted Data Set to its line in the
+	# legend so that user can interactively select which data to view
+	flowLines = []
+	winLines = []
+	pktdLines = []
 	
 	# plot flow rates, buff occup, packet loss for all links
 	# note: 'o' is to ensure scatterplot
 	for flow in flowsData.keys():
-		frates.plot(time, flowsData[flow]["Flow Rate (Mbps)"], '',
+		# note: the commas after assigning plotted line handle are necessary
+		fhandle, = frates.plot(time, flowsData[flow]["Flow Rate (Mbps)"], '',
 				label=str(flow),
 				markersize=2, markeredgewidth=0.0)
-		winsize.plot(time, flowsData[flow]["Window Size (pkts)"], '-',
+		whandle, = winsize.plot(time, flowsData[flow]["Window Size (pkts)"], '-',
 				label=str(flow),
 				markersize=2, markeredgewidth=0.0)
-		pktdelay.plot(time, flowsData[flow]["Packet Delay (ms)"], '-',
+		pdhandle, = pktdelay.plot(time, flowsData[flow]["Packet Delay (ms)"], '-',
 				label=str(flow),
 				markersize=2, markeredgewidth=0.0)
+
+		# update array of handles to plotted lines
+		flowLines.append(fhandle)
+		winLines.append(whandle)
+		pktdLines.append(pdhandle)
 	
 	# add titles, labels, and legends to plots; set lower bound of y axis
 	frates.set_xlabel('Time (ms)')
 	#frates.set_ylabel('Flow Rate (Mbps)')
-	frates.set_ylabel('Flow Percentage (Mbps)')
-	frates.legend(loc='best')
+	frates.set_ylabel('Flow Percentage (Mbps)') # useful for debugging purposes
+	fleg = frates.legend(loc='best')
 	frates.set_ylim(ymin=0)
 	
 	winsize.set_xlabel('Time (ms)')
 	winsize.set_ylabel('Window Size (pkts)')
-	winsize.legend(loc='best')
+	winleg = winsize.legend(loc='best')
 	winsize.set_ylim(ymin=0)
 	
 	pktdelay.set_xlabel('Time (ms)')
 	pktdelay.set_ylabel('Packet Delay (ms)')
-	pktdelay.legend(loc='best')
+	pktleg = pktdelay.legend(loc='best')
 	pktdelay.set_ylim(ymin=0)
 
 	# set window title
 	f.canvas.set_window_title("Flow Metrics Graph")
+
+	FlowDict = makeLegDict(fleg, flowLines)
+	wxDict = makeLegDict(winleg, winLines)
+	pdDict = makeLegDict(pktleg, pktdLines)
+
+	def onpick(event):
+	    # on the pick event, find the orig line corresponding to the
+	    # legend proxy line, and toggle the visibility
+	    legline = event.artist
+	    origline = FlowDict[legline]
+	    vis = not origline.get_visible()
+	    origline.set_visible(vis)
+	    # Change the alpha on the line in the legend so we can see what lines
+	    # have been toggled
+	    if vis:
+	        legline.set_alpha(1.0)
+	    else:
+	        legline.set_alpha(0.2)
+	    f.canvas.draw()
+
+	f.canvas.mpl_connect('pick_event', onpick)
 
 def plotAll(time, linksData, flowsData):
 	''' Generates plots for flow and link data'''
